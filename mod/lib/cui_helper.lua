268865475399594506a2bf6adfebd1ce662ec9e4
JLMin <2479297@qq.com>
2019-06-25 03:32:10 +0800

    add lib

---
+++ mod/lib/cui_helper.lua
@@ -0,0 +1,63 @@
+-- ===========================================================================
+-- Cui General Help Functions
+-- eudaimonia, 2/7/2019
+-- ===========================================================================
+
+isExpansion1 = Modding.IsModActive("1B28771A-C749-434B-9053-D1380C553DE9");
+isExpansion2 = Modding.IsModActive("4873eb62-8ccc-4574-b784-dda455e74e68");
+
+-- ===========================================================================
+function printc(t, i)
+  local n = i or "";
+  if isNil(t) then print("Cui Print:", n, "nil"); end
+
+  if type(t) == "table" then
+    local s = "-- Cui Print Table: " .. n .. " ================";
+    print(s); for k, v in pairs(t) do print("-", k, v); end print("--");
+  else
+    print("Cui Print:", n, t);
+  end
+end
+
+-- ===========================================================================
+function isNil(v)
+  if     type(v) == "table"  then return v == nil or next(v) == nil;
+  elseif type(v) == "string" then return v == nil or v == "";
+                             else return v == nil;
+  end
+end
+
+-- ===========================================================================
+function SortedTable(t, f)
+  local a = {};
+
+  for n in pairs(t) do table.insert(a, n); end
+
+  if f then table.sort(a, function(k1, k2) return f(t, k1, k2); end);
+       else table.sort(a);
+  end
+
+  local i = 0;
+  local iter =  function ()
+                  i = i + 1;
+                  if a[i] == nil then return nil;
+                                 else return a[i], t[a[i]];
+                  end
+                end
+  return iter;
+end
+
+-- ===========================================================================
+function CuiRegCallback(control, callbackLClick, callbackRClick, sound)
+  if callbackLClick then
+    control:RegisterCallback(Mouse.eLClick, callbackLClick);
+  end
+  if callbackRClick then
+    if sound then
+      control:RegisterCallback(Mouse.eRClick, function() callbackRClick(); UI.PlaySound(sound); end);
+    else
+      control:RegisterCallback(Mouse.eRClick, function() callbackRClick(); UI.PlaySound("Play_UI_Click"); end);
+    end
+  end
+  control:RegisterCallback(Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over"); end);
+end
---
+++ mod/lib/cui_settings.lua
@@ -0,0 +1,117 @@
+-- ===========================================================================
+-- Cui Settings Help Functions
+-- eudaimonia, 1/30/2019
+-- ===========================================================================
+
+local SETTING_PREFIX = "CUI_SETTING_";
+
+CuiSettings = {
+  -- map options
+  SHOW_IMPROVED       = { field = "ShowImproved",      default = true  },
+  SHOW_UNIT_FLAG      = { field = "ShowUnitFlags",     default = true  },
+  SHOW_TRADERS        = { field = "ShowTraders",       default = true  },
+  SHOW_CITY_BANNER    = { field = "ShowCityBanners",   default = true  },
+  -- map pins
+  SHOW_DISTRICT_ICONS = { field = "ShowDistrictIcons", default = true  },
+  SHOW_WONDER_ICONS   = { field = "ShowWonderIcons",   default = false },
+  AUTO_NAMING_PINS    = { field = "AutoNamingPins",    default = true  },
+  -- repost screen
+  SHOW_CITY_DETAILS   = { field = "ShowCityDetails",   default = false },
+  SHOW_STRATEGIC      = { field = "ShowStrategic",     default = true  },
+  SHOW_LUXURY         = { field = "ShowLuxury",        default = true  },
+  SHOW_BONUS          = { field = "ShowBonus",         default = false },
+  -- world tracker
+  HIDE_GOSSIP_LOG     = { field = "HideGossipLog",     default = false },
+  HIDE_COMBAT_LOG     = { field = "HideCombatLog",     default = false },
+  GOSSIP_LOG_PERSIST  = { field = "GossipLogPersist",  default = false },
+  COMBAT_LOG_PERSIST  = { field = "CombatLogPersist",  default = false },
+  GOSSIP_LOG_STATE    = { field = "GossipLogState",    default = 2     },
+  COMBAT_LOG_STATE    = { field = "CombatLogState",    default = 2     },
+  TECK_REMINDER       = { field = "TechReminder",      default = true  },
+  CIVICS_REMINDER     = { field = "CivicsReminder",    default = true  },
+  -- unit list
+  SHOW_UNIT_DETAILS   = { field = "ShowUnitDetails",   default = true  },
+  -- production panel
+  QUEUE_BY_DEFAULT    = { field = "QueueByDefault",    default = false },
+  -- great works
+  SORT_BY_CITY        = { field = "SortByCity",        default = true  },
+  -- civ assistant
+  SURPLUS_RESOURCE    = { field = "SurplusResource",   default = true  },
+  MAKE_PEACE          = { field = "MakePeace",         default = true  },
+  OPEN_BORDERS        = { field = "OpenBorders",       default = true  },
+  TRADE_ROUTES        = { field = "TradeRoutes",       default = true  },
+  WONDERS_TRACK       = { field = "WonderTrack",       default = true  },
+  SCIENCE             = { field = "ScienceVictory",    default = false },
+  CULTURE             = { field = "CultureVictory",    default = false },
+  DOMINATION          = { field = "DominationVictory", default = false },
+  RELIGION            = { field = "ReligionVictory",   default = false },
+  DIPLOMATIC          = { field = "DiplomaticVictory", default = false },
+};
+CuiSettings.__index = CuiSettings;
+
+-- ===========================================================================
+local function CuiCompleteKey(field)
+  return SETTING_PREFIX .. tostring(field);
+end
+
+-- ===========================================================================
+function CuiSettings:SetBoolean(k, b)
+  local key = CuiCompleteKey(k.field);
+  local value = b and "true" or "false";
+  PlayerConfigurations[Game.GetLocalPlayer()]:SetValue(key, value);
+end
+
+-- ===========================================================================
+function CuiSettings:GetBoolean(k)
+  local key = CuiCompleteKey(k.field);
+  local value = PlayerConfigurations[Game.GetLocalPlayer()]:GetValue(key);
+  if value ~= nil then
+    return value == "true";
+  else
+    return k.default;
+  end
+end
+
+-- ===========================================================================
+function CuiSettings:ReverseAndGetBoolean(k)
+  local v = CuiSettings:GetBoolean(k);
+  local value = v == false;
+  CuiSettings:SetBoolean(k, value);
+  return value;
+end
+
+-- ===========================================================================
+function CuiSettings:SetNumber(k, v)
+  local key = CuiCompleteKey(k.field);
+  local value = v;
+  PlayerConfigurations[Game.GetLocalPlayer()]:SetValue(key, value);
+end
+
+-- ===========================================================================
+function CuiSettings:GetNumber(k)
+  local key = CuiCompleteKey(k.field);
+  local value = PlayerConfigurations[Game.GetLocalPlayer()]:GetValue(key);
+  if value ~= nil then
+    return value;
+  else
+    return k.default;
+  end
+end
+
+-- ===========================================================================
+function CuiSettings:SetString(k, s)
+  local key = CuiCompleteKey(k.field);
+  local value = tostring(s);
+  PlayerConfigurations[Game.GetLocalPlayer()]:SetValue(key, value);
+end
+
+-- ===========================================================================
+function CuiSettings:GetString(k)
+  local key = CuiCompleteKey(k.field);
+  local value = PlayerConfigurations[Game.GetLocalPlayer()]:GetValue(key);
+  if value ~= nil then
+    return value;
+  else
+    return k.default;
+  end
+end
