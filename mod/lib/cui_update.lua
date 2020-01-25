a2f7691355f5012ea1c7a5c14de7b92015247a58
JLMin <2479297@qq.com>
2020-01-26 05:31:00 +0800

    version update

--- mod/lib/cui_update.lua
+++ mod/lib/cui_update.lua
@@ -1,5 +1,5 @@
-CuiVersion = "1.4.4"
-LastUpdate = "2020/01/17"
+CuiVersion = "1.4.5"
+LastUpdate = "2020/01/26"
 
 VersionDetail = "Concise UI - " .. CuiVersion .. "[NEWLINE]" ..
                 "Last Update: " .. LastUpdate
