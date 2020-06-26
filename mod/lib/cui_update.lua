4e96372d9bbb4d222051804f84fdf29dacff6ea4
JLMin <2479297@qq.com>
2020-06-26 18:50:30 +0800

    Update: Reactor Tooltip Fix

--- mod/lib/cui_update.lua
+++ mod/lib/cui_update.lua
@@ -1,3 +1,3 @@
-CuiVersion = "1.5.5"
-LastUpdate = "2020-06-07"
+CuiVersion = "1.5.6"
+LastUpdate = "2020-06-26"
 VersionDetail = "Concise UI - " .. CuiVersion .. "[NEWLINE]" .. "Last Update: " .. LastUpdate
